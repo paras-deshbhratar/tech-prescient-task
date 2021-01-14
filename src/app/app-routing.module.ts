afed185b0880fb59fe02d569d5976e0ffdd1cc33
Paras Deshbhratar <paras@alore.io>
2021-01-14 21:23:23 +0530

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) },
+  { path: '**', redirectTo: 'contacts'}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
